413f0a368141ee2eee0a1cf0b4038e8c7e6bcfe8
Sifon Isaac <syfonisaac@gmail.com>
2024-07-16 09:24:41 +0100

    mini app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center gap-8">   
-      <h1 className="text-2xl font-semibold">App to Help NKem convert JSon to CSV</h1>
+      <h1 className="text-2xl font-semibold">App to Help Nkem convert JSon to CSV</h1>
 
 
       <div>
