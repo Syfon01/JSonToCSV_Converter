23e08508bd04de71e100f63a75d849255239e2bc
Sifon Isaac <syfonisaac@gmail.com>
2024-07-16 09:35:22 +0100

    add some space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import JsonToCsvFile from "./component/jsonFile";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center gap-8">   
+      <div className="flex flex-col items-center justify-center gap-8 p-6">   
       <h1 className="text-2xl font-semibold">App to Help Nkem convert JSon to CSV</h1>
 
 
